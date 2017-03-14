219f5c91491cf78e80942294328da244aab276ed
Richard Patterson <detobate@gmail.com>
2017-03-14 11:14:54 +0000

    switched to SIGINT and added metadata

--- record_show.py
+++ record_show.py
@@ -2,7 +2,7 @@
 import requests
 import sys,os,signal
 import time,datetime
-import eyed3
+import taglib
 
 # Expects arguments --source-name=live_dj --source-status=false
 
@@ -13,6 +13,7 @@ CURRENT_URL="http://boosh.fm/current"
 
 def recordStream():
     current = ''
+    # Wait until we have the live show name
     while current[:5] != "Live:":
         r = requests.get(CURRENT_URL)
         current = r.text
@@ -22,21 +23,23 @@ def recordStream():
     r = requests.get(URL, stream=True)
     now = datetime.datetime.now()
     now = now.strftime("%Y%m%d")
-    FILENAME = RECORD_DIR+current+"-"+now+".mp3"
+    show = current+"-"+now
+    FILENAME = RECORD_DIR+show+".mp3"
     print("Recording: %s" % FILENAME)
     with open(FILENAME, 'wb') as f:
         try:
             for block in r.iter_content(1024):
                 f.write(block)
         except KeyboardInterrupt:
-            os.unlink(PIDFILE)
+            update_metadata(FILENAME, show)
+            os.unlink(PIDFILE) # Clean up PID file
 
-    writeID3(FILENAME, current)
 
-def writeID3(file, title):
-    show = eyed3.load(file)
-    show.tag.title = title
-    show.tag.save()
+def update_metadata(filename, show):
+    print("Updating Metadata of %s with %s" % (filename, show))
+    file = taglib.File(filename)
+    file.tags['TITLE'] = [show]
+    file.save()
 
 
 def main():
@@ -45,13 +48,14 @@ def main():
         with open(PIDFILE, "r") as p:
             PID=p.readline()
         print("Killing %s" % PID)
-        os.kill(int(PID), signal.SIGTERM)
-        os.unlink(PIDFILE)
+        os.kill(int(PID), signal.SIGINT)
+
     elif sys.argv[1] == "--source-name=live_dj" and sys.argv[2] == "--source-status=true" and os.path.isfile(PIDFILE):
         print("Error: Already recording.")
         with open(PIDFILE, "r") as p:
             print("PID: %s" % p.readline())
         exit(1)
+
     elif sys.argv[1] == "--source-name=live_dj" and sys.argv[2] == "--source-status=true" and not os.path.isfile(PIDFILE):
         PID = str(os.getpid())
         print("Creating pid lock file")
@@ -59,7 +63,6 @@ def main():
             f.write(PID)
         print("Looking for live DJ")
         recordStream()
-        os.unlink(PIDFILE)
 
     elif sys.argv[1] == "--source-name=live_dj" and sys.argv[2] == "--source-status=false" and not os.path.isfile(PIDFILE):
         print("Nothing to do")
